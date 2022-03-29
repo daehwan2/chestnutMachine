28ddb2e355f2533897f99d95dce554903af8a9b1
daehwan2 <dhdh0482@naver.com>
2022-03-29 23:54:14 +0900

    Fix: Ranking 페이지 라우트 버그수정

--- src/components/RankingButton.jsx
+++ src/components/RankingButton.jsx
@@ -1,13 +1,18 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 function RankingButton() {
+  const navigate = useNavigate();
+  const onClickButton = () => {
+    navigate("/ranking");
+  };
   return (
     <div>
-      <a
-        href="/ranking"
+      <button
+        onClick={onClickButton}
         className="flex justify-center items-center bg-[white] text-[20px] text-center w-[280px] h-[60px] border-[1px] border-black rounded-[10px] drop-shadow-lg">
         <strong>랭킹 보기</strong>
-      </a>
+      </button>
     </div>
   );
 }
