d4a8203f2a4d4a3d1cd980913cfd25094a3f0de3
daehwan2 <dhdh0482@naver.com>
2022-06-15 14:13:04 +0900

    Feat: 랭킹 자기점수표시

--- src/routes/Ranking.jsx
+++ src/routes/Ranking.jsx
@@ -1,23 +1,37 @@
 import React, { useEffect, useState } from "react";
+import { AiFillStar } from "react-icons/ai";
 
-function Ranking({ scoresObj }) {
+function Ranking({ scoresObj, userObj }) {
   const [result, setResult] = useState([]);
   useEffect(() => {
     setResult(scoresObj.sort((a, b) => b.score - a.score));
   }, [scoresObj]);
   return (
-    <div className="flex flex-col justify-center items-center py-[50px] w-[100%] md:w-[500px] m-auto">
+    <div className="flex flex-col justify-center items-center py-[50px] w-[100%] md:w-[500px] m-auto h-[100%]">
       <strong className="text-[24px]">랭킹</strong>
-
-      <ol className="flex flex-col text-center">
-        <li className="flex">
+      <div className="flex flex-col text-center">
+        <div className="flex">
           <div className="w-[50px]">순위</div>
           <div className="w-[70px]">이름</div>
           <div className="w-[50px]">점수</div>
           <div className="w-[170px]">플레이시간</div>
-        </li>
+        </div>
+      </div>
+      <ol className="flex flex-col text-center h-[100%] overflow-y-auto overflow-x-visible">
         {result.map((score, i) => (
-          <li className="flex">
+          <li
+            className={
+              score.uid === userObj.uid
+                ? "flex relative border-[red] border-[1px]"
+                : "flex relative"
+            }>
+            {score.uid === userObj.uid ? (
+              <div className="absolute flex items-center justify-center text-[blue]">
+                <strong className="text-[16px]">Me</strong>
+              </div>
+            ) : (
+              <></>
+            )}
             <div className="w-[50px]">{i + 1}</div>
             <div className="w-[70px]">{score.name}</div>
             <div className="w-[50px] h-[100%] text-[red] bg-[black] rounded-[4px] flex justify-center items-center">
